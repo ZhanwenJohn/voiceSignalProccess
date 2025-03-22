aa9cff018690634c12777f0f4a792ca571138599
ZhaoZhanwen Desktop MSI <johnzhanwen@gmail.com>
2025-03-22 10:49:02 +0800

    fix(work1):完成实验一的报告;fix(work2):完善了语音信号加窗分帧程序,add(work2):添加了语谱图和清浊音分析程序

--- work_2/voiceFrame.m
+++ work_2/voiceFrame.m
@@ -9,44 +9,50 @@ if size(y, 2) > 1
     y = mean(y, 2);
 end
 
-%% 设定帧长和帧移
-frame_duration = 0.01;
+%% 设定帧长
+frame_duration = 0.05;
 frame_length = frame_duration * fs;
 fprintf('计算窗口大小: %d\n', frame_length);
-% 计算帧长
-if mod(log2(frame_length), 1) == 0
-    frame_length = frame_length * 2;
-else
-    frame_length = 2^(ceil(log2(frame_length)));
-end
-fprintf('实际窗口大小: %d\n', frame_length);
-start_idx = 120000;
-end_idx = start_idx + frame_length - 1;
+start_id = 12512;
 
-%% 生成汉明窗
-hamming_window = hamming(frame_length, 'periodic');
+%% 生成海明窗
+hamming_window = hamming(frame_length);
 
-%% 汉明窗处理帧
-frames = y(start_idx:end_idx)' .* hamming_window';
+%% 海明窗处理帧
+hm_frames = y(start_id:start_id+frame_length-1)' .* hamming_window';
 
-%% 绘制汉明窗
+%% 绘制海明窗窗
 figure;
-plot((0:frame_length-1) / fs, frames);
+subplot(2,1,1);
+plot((0:frame_length-1) / fs, hm_frames);
 title('Hamming Window Wave');
-xlabel('时间 (秒)');
-ylabel('振幅');
-grid on;
+xlabel('sample point');
+ylabel('Amplitude');
 
 %% 生成矩形窗
+% rectangular_length = frame_length / 4;
 rectangular_window = rectwin(frame_length);
-
-%% 矩形窗处理帧
-frames = y(start_idx:end_idx)' .* rectangular_window';
+rectangular_frams = y(start_id:start_id+frame_length-1)' .* rectangular_window';
 
 %% 绘制矩形窗
-figure;
-stem((0:frame_length-1) / fs, rectangular_window, 'b', 'filled');
-xlabel('样本点');
-ylabel('幅度');
+subplot(2,1,2);
+stem((0:frame_length-1) / fs, rectangular_frams);
+xlabel('sample point');
+ylabel('Amplitude');
 title('Rectangular Window Wave');
-grid on;
+
+%% 绘制语谱图
+figure;
+spectrogram(y);
+title('Spectrogram');
+
+%% 清浊音分析
+time = (0:length(y)-1) / fs;
+energy = sum(y.^2) / length(y);
+zcr = sum(abs(diff(sign(y)))) / length(y);
+
+if energy > 0.01 && zcr < 0.1
+    disp('语音信号中存在清音');
+else
+    disp('语音信号中存在浊音');
+end
