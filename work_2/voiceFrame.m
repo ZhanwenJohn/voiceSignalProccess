36f9258f1fb5c946fc7350f0f70f57c10aa890b9
ZhaoZhanwen Lenovo Air-15 2019 <1363035157@qq.com>
2025-03-29 11:40:53 +0800

    fix(work2):补充了分窗和特征部分代码，完成了报告

--- work_2/voiceFrame.m
+++ work_2/voiceFrame.m
@@ -10,10 +10,17 @@ if size(y, 2) > 1
 end
 
 %% 设定帧长
-frame_duration = 0.05;
+frame_duration = 0.1;
 frame_length = frame_duration * fs;
 fprintf('计算窗口大小: %d\n', frame_length);
-start_id = 12512;
+% 计算帧长
+if mod(log2(frame_length), 1) == 0
+    frame_length = frame_length * 2;
+else
+    frame_length = 2^(ceil(log2(frame_length)));
+end
+fprintf('最终窗口大小: %d\n', frame_length);
+start_id = 8256;
 
 %% 生成海明窗
 hamming_window = hamming(frame_length);
@@ -30,13 +37,14 @@ xlabel('sample point');
 ylabel('Amplitude');
 
 %% 生成矩形窗
-% rectangular_length = frame_length / 4;
-rectangular_window = rectwin(frame_length);
-rectangular_frams = y(start_id:start_id+frame_length-1)' .* rectangular_window';
+rectangular_length = frame_length / 2;
+start_id = start_id + (rectangular_length / 2);
+rectangular_window = rectwin(rectangular_length);
+rectangular_frams = y(start_id:start_id+rectangular_length-1)' .* rectangular_window';
 
 %% 绘制矩形窗
 subplot(2,1,2);
-stem((0:frame_length-1) / fs, rectangular_frams);
+stem((0:rectangular_length-1) / fs, rectangular_frams);
 xlabel('sample point');
 ylabel('Amplitude');
 title('Rectangular Window Wave');
@@ -44,7 +52,7 @@ title('Rectangular Window Wave');
 %% 绘制语谱图
 figure;
 spectrogram(y);
-title('Spectrogram');
+title('语谱图');
 
 %% 清浊音分析
 time = (0:length(y)-1) / fs;
@@ -56,3 +64,4 @@ if energy > 0.01 && zcr < 0.1
 else
     disp('语音信号中存在浊音');
 end
+
